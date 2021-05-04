a014d066391ae8b22688615a908d7c606b9c4ffa
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-05-04 13:22:10 +0100

    Adding Models to model.py
    Implemented models to models.py for actors and movies

--- starter/models.py
+++ starter/models.py
@@ -1,2 +1,17 @@
 import os
-from flask_sqlalchemy import SQLalchemy
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password123@localhost:5432/capstone'
+
+class Movies(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    title = db.Column(db.string(120), unique=True, nullable=False)
+    release_date = db.Column(db.Datetime, nullable=False)
+
+class Actors(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    name = db.Column(db.string(120), nullable=False)
+    age = db.Column(db.integer, nullable=False)
+    gender = db.Column(db.string(120), nullable=False)
