6f45c5e0edbe2c4a77761bd037c945041f2cb6cd
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-05-11 14:21:25 +0100

    Indentation error

--- starter/auth.py
+++ starter/auth.py
@@ -19,34 +19,33 @@ class AuthError(Exception):
 
 
 def get_token_auth_header():
-    """Obtains the Access Token from the Authorization Header
-     """
-      auth_header = request.headers.get('Authorization', None)
+    """Obtains the Access Token from the Authorization Header """
+    auth_header = request.headers.get('Authorization', None)
 
-       if not auth_header:
-            raise AuthError({"code": "authorization_header_missing",
-                             "description": "Authorization header is expected"}, 401)
+    if not auth_header:
+        raise AuthError({"code": "authorization_header_missing",
+                        "description": "Authorization header is expected"}, 401)
 
-        header_parts = auth_header.split(' ')
+    header_parts = auth_header.split(' ')
 
-        if header_parts[0].lower() != 'bearer':
-            raise AuthError({"code": "invalid_header",
+    if header_parts[0].lower() != 'bearer':
+        raise AuthError({"code": "invalid_header",
                             "description":
                                 "Authorization header must start with"
                                 " Bearer"}, 401)
 
-        elif len(header_parts) == 1:
-            raise AuthError({"code": "invalid_header",
-                            "description": "Token not found"}, 401)
+    elif len(header_parts) == 1:
+        raise AuthError({"code": "invalid_header",
+                        "description": "Token not found"}, 401)
 
-        elif len(header_parts) > 2:
-            raise AuthError({"code": "invalid_header",
-                            "description":
-                                "Authorization header must be"
-                                " Bearer token"}, 401)
+    elif len(header_parts) > 2:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Authorization header must be"
+                            " Bearer token"}, 401)
 
-        token = header_parts[1]
-        return token
+    token = header_parts[1]
+    return token
 
 
 def verify_decode_jwt(token):
