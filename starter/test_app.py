7afc7671e0c904dc40baa61b189b172cb120cbbb
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-05-13 13:24:49 +0100

    Updated setup method
    updated setup method

--- starter/test_app.py
+++ starter/test_app.py
@@ -7,12 +7,17 @@ from flask_sqlalchemy import SQLAlchemy
 from app import create_app
 from models import setup_db, Movies, Actors
 
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'password123')
+DB_NAME = os.getenv('DB_NAME', 'capstone_test')
 
-class AppTestCase(unittest.Testcase):
+class CapstoneTestCase(unittest.Testcase):
     
 
     def setUp(self):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = 'capstone_test'
-        self.database_path = "postgres://{}:{}@{}/{}".format()
+        self.database_path = "postgres://{}:{}@{}/{}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+        setup_db(self.app, self.database_path)
