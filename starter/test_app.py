38ac9d0f12b3e1a775c4441204f4045cf261c1c0
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-05-13 13:07:07 +0100

    Imports and defined setup method
    Added more dependencies and defined setup method

--- starter/test_app.py
+++ starter/test_app.py
@@ -1,9 +1,18 @@
+import os
 import unittest
 import json
 
+from flask_sqlalchemy import SQLAlchemy
+
 from app import create_app
-from models import setup_db
+from models import setup_db, Movies, Actors
 
 
 class AppTestCase(unittest.Testcase):
-    pass
+    
+
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = 'capstone_test'
+        self.database_path = "postgres://{}:{}@{}/{}".format()
