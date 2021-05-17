e8f4d3a36ff65fae7bcb6d28120e659479917360
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-05-17 12:51:52 +0100

    Syntax error

--- starter/auth.py
+++ starter/auth.py
@@ -49,7 +49,7 @@ def get_token_auth_header():
 
 
 def verify_decode_jwt(token):
-    jsonurl = urlopen(f"https://" + {AUTH0_DOMAIN} + "/.well-known/jwks.json")
+    jsonurl = urlopen(f"https://{AUTH0_DOMAIN}/.well-known/jwks.json")
     jwks = json.loads(jsonurl.read())
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
