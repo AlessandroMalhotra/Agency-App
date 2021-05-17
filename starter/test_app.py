5d3422e13c52a2a701bfc5fe00125595fa192f1f
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-05-17 15:23:45 +0100

    Updated syntax for database path

--- starter/test_app.py
+++ starter/test_app.py
@@ -26,7 +26,7 @@ class CapstoneTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = 'capstone_test'
-        self.database_path = "postgres://{}:{}@{}/{}".format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+        self.database_path = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
         setup_db(self.app, self.database_path)
     
         with self.app.app_context():
