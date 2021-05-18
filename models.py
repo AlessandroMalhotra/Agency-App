add123f92714f77393af717e65b57320f7528948
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-05-18 23:27:12 +0100

    change db path

--- models.py
+++ models.py
@@ -1,16 +1,22 @@
 import os
-import re
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-uri = os.environ.get('DATABASE_URL')
 
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PORT = os.getenv('DB_PORT', 5432)
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'password123')
+DB_NAME = os.getenv('DB_NAME', 'capstone')
+
+DB_PATH = 'postgresql://{}:{}@{}:{}/{}'.format(
+    DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
 
 db = SQLAlchemy()
 
 
-def setup_db(app, database_path=uri):
+def setup_db(app, database_path=DB_PATH):
     app.config['SQLALCHEMY_DATABASE_URI'] = database_path
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
