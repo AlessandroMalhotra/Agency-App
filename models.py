977eae9976de1b3666fb3421b580484ea1cb2895
AlessandroMalhotra <68227293+AlessandroMalhotra@users.noreply.github.com>
2021-05-20 20:59:18 +0100

    Updated database path

--- models.py
+++ models.py
@@ -4,12 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
-DB_USER = os.getenv('DB_USER', 'postgres')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'password123')
-DB_NAME = os.getenv('DB_NAME', 'capstone')
-
-DB_PATH =  'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+DB_PATH = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) 
 
 db = SQLAlchemy()
 
